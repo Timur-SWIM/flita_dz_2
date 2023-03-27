48bcfb540ad251468929398c836796b1fe7e2477
timur <tmyaldzin13@gmail.com>
2023-03-27 13:04:57 +0300

    add matrix deskription

--- main.c
+++ main.c
@@ -1,3 +1,6 @@
+/* таблица в структуре
+ * ввод файла через аргументы*/
+
 #include "main.h"
 
 int main() {
@@ -5,23 +8,33 @@ int main() {
 
     inputf = fopen("adjacency_matrix_0.txt", "r");
     outf = fopen("output_file.dot", "w");
-    if (inputf == NULL || outf == NULL){
+    if (inputf == NULL || outf == NULL) {
         puts("Error: file not found");
-        return -1;}
+        return -1;
+    }
     char arr[11];
+    int size_matrix = 0, str_len;
 
-    fputs("digraph graf1{", outf);
+    while (fgets(arr, 100, inputf) != NULL){
+        ++size_matrix;
+    }
+    size_matrix-=1;
+    str_len = (size_matrix * 2) + 1;
+    printf("counter -> %d\n", size_matrix);
+    printf("str_len->%d\n", str_len);
+
+    fputs("graph graf1{", outf);
     fputs("\n", outf);
 
-    for (int i = 1; i < SIZE_MATRIX + 1; i++){
-        fgets(arr, ARRLEN,inputf);
+    for (int i = 1; i < size_matrix + 1; i++){
+        fgets(arr, str_len,inputf);
         deblank(arr);
-        for  (int j = 0; j < SIZE_MATRIX; j++){
+        for  (int j = 0; j < size_matrix; j++){
             if (arr[j] == '1'){
-                fprintf(outf, "%d->%d\n", i, j + 1);
+                fprintf(outf, "%d--%d\n", i, j + 1);
             }
             if(arr[j] > '1'){
-                fprintf(outf, "%d->%d \n", i, j + 1);
+                fprintf(outf, "%d--%d [weight=%c, penwidth=%c]\n", i, j + 1, arr[j], arr[j]);
             }
         }
     }
--- main.h
+++ main.h
@@ -2,7 +2,6 @@
 #ifndef DZ_2_MAIN_H
 #define DZ_2_MAIN_H
 #define ARRLEN 11
-#define SIZE_MATRIX 5
 #include <stdio.h>
 #include <stdlib.h>
 #include "deblank.h"
